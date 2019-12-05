4c92d1315f298a0834bf41a667a5966b30bb16c4
RicardasSim <universe.nomad@gmail.com>
2019-12-05 21:58:00 +0200

    add threadf.h

---
+++ threadf.h
@@ -0,0 +1,16 @@
+#ifndef THREADF_H
+#define THREADF_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint64_t TestFnAsm ( int8_t*, uint64_t , int8_t*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // THREADF_H
